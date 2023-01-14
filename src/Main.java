f2fc65f9c445fd977cebca4d3133b3b9f2236895
Aleks <alx49b@gmail.com>
2023-01-14 14:22:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+    char i = 'y' ;
+    int a = 12;
+    double b = 2.6;
+    float c = 2.5f;
+    byte d = 122;
+    boolean e = false;
+    short f = 3456;
+    long j = 21;
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(j);
+
+    }
+
+}
